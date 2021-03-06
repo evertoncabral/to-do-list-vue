9b9ebe2c090735db9ba655d8e4fadb926358e19c
Everton Cabral <evertoncab@gmail.com>
2021-03-06 15:44:01 -0300

    adicionando dados do input

---
+++ projeto/src/components/ToDoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>to do list com vue</h1>
+    <input v-model.trim="task" />
+    <button type="button" @click="addTasks">Adicionar</button>
+    {{ task }}
+    <ul>
+      <li v-for="obj in tasks" :key="obj">{{ obj }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDoList",
+  data() {
+    return {
+      task: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTasks() {
+      this.tasks.push(this.task);
+      this.task = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
